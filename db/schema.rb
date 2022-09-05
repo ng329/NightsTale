8ec96530ac4c1d0a65bb173b45bf11974050591a
Natasha Gill <natashamgill@hotmail.co.uk>
2022-09-05 17:49:41 +0100

    added column tags to events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_05_154247) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_05_164903) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,6 +65,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_05_154247) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.string "tags"
   end
 
   create_table "favourites", force: :cascade do |t|
