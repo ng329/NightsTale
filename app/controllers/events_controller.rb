1d1cb5341f717b726b1d0d7629025c3e0982b7d5
Natasha Gill <natashamgill@hotmail.co.uk>
2022-09-05 16:08:08 +0100

    map added to index page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,15 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.near()
+    @events = Event.all
+
+    @markers = @events.geocoded.map do |event|
+      {
+        lat: event.latitude,
+        lng: event.longitude,
+        info_window: render_to_string(partial:
+          "info_window", locals: {event: event})
+      }
+    end
   end
 
   def show
