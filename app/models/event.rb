3369e1ed394df45542dc71a883d828e8a125b7c4
Natasha Gill <natashamgill@hotmail.co.uk>
2022-09-06 11:58:33 +0100

    errors with linking

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,6 +4,7 @@ class Event < ApplicationRecord
 
   has_many :favourites, dependent: :destroy
   has_many :users, through: :bookings
+  has_many :bookings
   has_many_attached :photos
 
   geocoded_by :location
