de923ad828e0da95fe16dbb4b118b9ecb0df54d6
Natasha Gill <natashamgill@hotmail.co.uk>
2022-09-06 20:07:26 +0100

    set up favourites controller

---
+++ app/javascript/controllers/favourites_controller.js
@@ -0,0 +1,28 @@
+import { Controller } from "@hotwired/stimulus"
+import { end } from "@popperjs/core"
+
+// Connects to data-controller="favourites"
+export default class extends Controller {
+  static targets = ["button"]
+
+  connect() {
+    console.log("connected")
+  }
+
+  togglefavourite(event) {
+    console.log(event)
+    if (this.buttonTarget.classList.contains('added')){
+      this.buttonTarget.classList.remove("added")
+      this.buttonTarget.classList.remove("fa-solid")
+      this.buttonTarget.classList.add("fa-regular")
+      // send to destroy
+    } else {
+      this.buttonTarget.classList.add("added")
+      this.buttonTarget.classList.remove("fa-regular")
+      this.buttonTarget.classList.add("fa-solid")
+      // send to add
+    }
+
+
+  }
+}
