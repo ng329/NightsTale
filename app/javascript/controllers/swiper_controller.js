91c643c117930005775573674cf57e4c5c5f6fb6
Edd Shotton <edward.shotton@gmail.com>
2022-09-07 12:52:08 +0100

    swiper implemented, basic styling done.

--- app/javascript/controllers/swiper_controller.js
+++ app/javascript/controllers/swiper_controller.js
@@ -1,7 +1,27 @@
-import { Controller } from "@hotwired/stimulus"
-import Carousel from "stimulus-carousel"
+import { Controller } from "@hotwired/stimulus";
+import Swiper, { Navigation, Pagination} from "swiper";
+// import 'swiper/css/bundle';
+// import 'swiper/css';
+// import 'swiper/css/pagination'
+
+// import Carousel from "stimulus-carousel"
 // Connects to data-controller="swiper"
 export default class extends Controller {
   connect() {
+    const swiper = new Swiper('.swiper', {
+      speed: 400,
+      spaceBetween: 100,
+
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    }
+    
+    );
+    console.log(swiper)
   }
 }
