49d3c2e30b651f38b05dcb471c082f7ca364773f
Natasha Gill <natashamgill@hotmail.co.uk>
2022-09-08 11:22:42 +0100

    modal for booking data functioning

--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -3,22 +3,38 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="calendar"
 export default class extends Controller {
   static targets = ["booking", "modal"]
+
+  // static values = {
+  //   eventId: Number,
+  //   title: String,
+  //   location: String,
+  //   date: String,
+  //   price: Number,
+  //   people: Number
+  // }
   connect() {
     console.log("connect")
-    console.log(this.bookingTarget)
+    console.log("4")
   }
 
-  // appear() {
-  //   if (this.modalTarget.style.display === "none") {
-  //     this.modalTarget.style.display = "block";
-  //   } else {
-  //     this.modalTarget.style.display = "none";
-  //   }
+  appear() {
+    // console.log(this.bookingTarget.dataset.eventid)
 
-  //   const bookingInfo = `<div class="container">
-  //                     <h6></h6>
-  //                     <p>${this.bookingTarget}</p>
-  //                 </div>`
-  //   this.modalTarget.insertAdjacentHTML("beforeend", bookingInfo)
-  // }
+    if (this.modalTarget.style.display === "none") {
+      this.modalTarget.style.display = "block";
+      const bookingInfo = `<div class="container">
+                          <a href="/events/${this.bookingTarget.dataset.eventid}">
+                            <h6>${this.bookingTarget.dataset.title}</h6>
+                            <p>Location: ${this.bookingTarget.dataset.location}</p>
+                            <p>Start time: ${this.bookingTarget.dataset.date}</p>
+                            <p>Number of people: ${this.bookingTarget.dataset.people}</p>
+                            <p>Total price: ${this.bookingTarget.dataset.price}</p>
+                          </a>
+                    </div>`
+      this.modalTarget.innerHTML = bookingInfo;
+    } else {
+      this.modalTarget.style.display = "none";
+    }
+
+  }
 }
