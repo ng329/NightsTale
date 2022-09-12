9afdee4266e3654e7543419fe2d2b40bef1c9d8d
Natasha Gill <natashamgill@hotmail.co.uk>
2022-09-12 15:18:20 +0100

    friends added and working with pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :users, only: %i[show]
+  resources :users, only: %i[show] do
+    resources :invitations, only: %i[index create destroy update new]
+  end
 
   get "popular", to: "events#popular"
+
 
   resources :events, only: %i[index show] do
     resources :bookings, only: %i[create]
