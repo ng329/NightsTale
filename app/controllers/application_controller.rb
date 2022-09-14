94012ad5ad89578fe4c6d47436be0ac7bc926ccb
Natasha Gill <natashamgill@hotmail.co.uk>
2022-09-14 16:53:20 +0100

    add meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,8 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for scope
     root_path
   end
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
