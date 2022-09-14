f63f9d711f773d81f402d06758be834e958d9dc3
Natasha Gill <natashamgill@hotmail.co.uk>
2022-09-14 14:06:45 +0100

    booking currency

--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -9,7 +9,7 @@ export default class extends Controller {
   }
 
   appear() {
-    // console.log(this.bookingTarget.dataset.eventid)
+    // console.log();
     if (this.modalTarget.style.display === "none") {
       this.modalTarget.style.display = "block";
       const bookingInfo = `<div class="container">
@@ -18,7 +18,7 @@ export default class extends Controller {
                             <p>Location: ${this.bookingTarget.dataset.location}</p>
                             <p>Start time: ${this.bookingTarget.dataset.date}</p>
                             <p>Number of people: ${this.bookingTarget.dataset.people}</p>
-                            <p>Total price: ${this.bookingTarget.dataset.price}</p>
+                            <p>Total price: ${new Intl.NumberFormat('ja-JP', { style: 'currency', currency: 'GBP' }).format(this.bookingTarget.dataset.price)}</p>
                           </a>
                           <a href="/users/${this.bookingTarget.dataset.userid}">
                             <p>Individual who booked: ${this.bookingTarget.dataset.userbooked}</p>
